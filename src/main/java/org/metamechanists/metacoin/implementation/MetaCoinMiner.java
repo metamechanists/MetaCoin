23b82358468885ea6ecc12f91f02db213ed75f28
LordIdra <percy.baker.0@gmail.com>
2024-03-26 22:40:28 +0000

    display model work

--- src/main/java/org/metamechanists/metacoin/implementation/MetaCoinMiner.java
+++ src/main/java/org/metamechanists/metacoin/implementation/MetaCoinMiner.java
@@ -520,7 +520,7 @@ public class MetaCoinMiner extends DisplayModelBlock implements Sittable {
                         .brightness(15)
                         .material(Material.WHITE_CONCRETE)
                         .size(0.35F, 2.5F, 0.35F)
-                        .location(0.75F, 4.0F, 0.0F)
+                        .location(0.8F, 3.8F, 0.0F)
                         .rotation(0, 0, Math.PI * 1 / 6))
                 .add("bottom-slanted-pillar-2", new ModelCuboid()
                         .brightness(15)
